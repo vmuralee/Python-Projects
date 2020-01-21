3fb449c9ccac8fb387eec549af07935f10e91f92
vinay krishnan <vinaykrishnanmb@gmail.com>
2020-01-21 13:34:10 +0530

    cplus directory added

---
+++ Computaional_tutorial/cplus/Integration.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+
+using namespace std;
+
+double func(double x){
+
+  return x*x;
+}
+
+
+double rectangle_rule(double a, double b, int n, double (*func)(double))
+{
+  double rectangle_sum;
+  double fa, fb, x, step;
+  int j;
+  step=(b-a)/((double) n);
+  rectangle_sum=0.;
+  for (j = 0; j <= n; j++){
+    x = (j+0.5)*step+a; // midpoint of a given rectangle
+    rectangle_sum+=(*func)(x); // add value of function.
+  }
+  rectangle_sum *= step; // multiply with step length.
+  return rectangle_sum;
+}
+double trapezoidal_rule(double a, double b, int n, double (*func)(double))
+{
+  double trapez_sum=0;
+  double fa, fb, x, step;
+  int j;
+  step=(b-a)/((double) n);
+  fa=(*func)(a)/2. ;
+  fb=(*func)(b)/2. ;
+  
+  for (j=1; j <= n-1; j++){
+    x=j*step+a;
+    trapez_sum+=(*func)(x);
+  }
+  trapez_sum=(trapez_sum+fb+fa)*step;
+  return trapez_sum;
+}
+int main(){
+  cout<<trapezoidal_rule(0,1,10,func)<<endl;
+  cout<<rectangle_rule(0,1,100,func)<<endl;
+}
