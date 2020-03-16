c8c2864f6f2e523e916d26f991a584f26272a811
vinay krishnan <vinaykrishnanmb@gmail.com>
2020-03-16 11:39:03 +0530

    input commands added

--- Cracking/morse_code.py
+++ Cracking/morse_code.py
@@ -5,7 +5,7 @@ def alphabets(keys):
               'G':'--.','H':'....','I':'..','J':'.---','K':'-.-','L':'.-..',
               'M':'--','N':'-.','O':'---','P':'.--.','Q':'--.-','R':'.-.',
               'S':'...','T':'-','U':'..-','V':'...-','W':'.--','X':'-..-',
-              'Y':'-.--','Z':'--..'}
+              'Y':'-.--','Z':'--..',' ':'*'}
     return alphas[keys] 
 def codes():
     alphas = {'A':'.-','B':'-...','C':'-.-.','D':'-..','E':'.','F':'..-.',
@@ -32,3 +32,19 @@ def Decrypt(gibberish):
     print(''.join([ele for ele in w]))
             
 
+entry = input('Do you want to Encrypt press 0 or to Decrypt press 1 : \n')
+if(entry == '0'):
+    rerun = 'y'
+    while(rerun=='y'):
+        message = input('Enter the message: ')
+        Encrypt(message)
+        rerun = input('Do you want to continue? [y/n] ')
+elif(entry == '1'):
+    rerun = 'y'
+    while(rerun=='y'):
+        message = input('Enter your giberish: ')
+        Decrypt(message)
+        rerun = input('Do you want to continue? [y/n] ')
+else:
+    print('Please enter a valid key')
+    exit()
